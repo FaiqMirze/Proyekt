cb423edea60df9e907fd8062d15f9c9655967f9f
FaiqMirze <faiqmirzeyev94@gmail.com>
2024-09-17 21:40:51 +0400

    yeni

--- app/Http/Controllers/FrontController.php
+++ app/Http/Controllers/FrontController.php
@@ -2,13 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Article;
 use Illuminate\Http\Request;
 
 class FrontController extends Controller
 {
     public function index()
     {
-
-        return view('front.index');
+        $articles=Article::all();
+        return view('front.index',compact('articles'));
     }
 }
